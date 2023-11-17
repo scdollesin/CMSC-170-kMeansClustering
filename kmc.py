7c29e3f74af0700cbaf70d08e8779e9eff176e85
scdollesin <scdollesin@up.edu.ph>
2023-11-17 13:42:37 +0800

    feat: input validation for k and attributes

--- kmc.py
+++ kmc.py
@@ -1,25 +1,38 @@
+
 import random
 
+
 def selectAttribute(attributes):
-    for i in range(len(attributes)):
+    no_of_attribs = len(attributes)
+    for i in range(no_of_attribs):
         print(f"[{i}] {attributes[i]}")
-    choice = int(input("Select an attribute: "))
+    choice = 1
+
+    while (1):
+        choice = int(input("Select an attribute: "))
+        if (choice > (no_of_attribs - 1)) or (choice < 0):
+            print("Please enter a a valid attribute")
+        else:
+            break
+
     return choice
+
 
 def getData(file):
     dataset = file.read().split("\n")
     for i in range(len(dataset)):
         dataset[i] = dataset[i].split(",")
         for j in range(len(dataset[1])):
-            if (i>0):
+            if (i > 0):
                 dataset[i][j] = float(dataset[i][j])
         #print(dataset[i])
-    attributes = dataset.pop(0)   #separate the attributes from the actual data
+    attributes = dataset.pop(0)  #separate the attributes from the actual data
     #print(attributes)
     #print("No. of points: ", len(dataset))
-    
+
     return attributes, dataset
-    
+
+
 inputcsv = open("08 Data Wine.csv")
 if (inputcsv.readable()):
     #read input file
@@ -27,17 +40,22 @@ if (inputcsv.readable()):
     #ask for 1st and 2nd attribute and the value of k
     att1 = selectAttribute(attributes)
     att2 = selectAttribute(attributes)
-    k = int(input("Enter value of k: "))
-    
-    #determine centroids from the dataset randomly
+
+    k = 1
+    while (1):
+        k = int(input("Enter value of k: "))
+        if (k > 10):
+            print("10 maximum centroids allowed")
+        elif (k < 1):
+            print("Please enter a value greater than 0")
+        else:
+            break
+
     centroids = []
     for i in range(k):
         c = []
-        print(len(dataset))
-        r = random.randint(1, (len(dataset)-1))
+        r = random.randint(1, (len(dataset) - 1))
         c.append(dataset[r][att1])
         c.append(dataset[r][att2])
         print(f"centroid {i}: {c[0]}, {c[1]} [{r}]")
         centroids.append(c)
-    
-    
